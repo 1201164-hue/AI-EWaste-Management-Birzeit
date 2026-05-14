3f9cc1ff24f8a9d5704f58c48d19b073e36b9cf9
1201164-hue <1201164@student.birzeit.edu>
2026-05-14 06:05:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+from flask_cors import CORS
 import pandas as pd
 import joblib
 
@@ -26,7 +26,7 @@ features = joblib.load(
 # ==========================
 
 app = Flask(__name__)
-
+CORS(app)
 
 # ==========================
 # Categorize device
